86c44b722a002a02ba5fd64c3d2fd767c3b5b482
ekamtaneja <95505981+ekamtaneja@users.noreply.github.com>
2024-10-02 00:31:28 -0700

    Fixed repeat key functionality. Added escape key functionality with FN_NUM Tap Dance.
    keyboards/kbdfans/kbd67/mkiirgb/keymaps/ekamtaneja/keymap.c
    Fixed repeat key functionality. Added KC_ESC functionality with FN_NUM
    Tap Dance. Renamed TD keycode as well as few functions to suit the
    update. Added comments.

--- keyboards/kbdfans/kbd67/mkiirgb/keymaps/ekamtaneja/keymap.c
+++ keyboards/kbdfans/kbd67/mkiirgb/keymaps/ekamtaneja/keymap.c
@@ -23,10 +23,13 @@
 #define _FN 3
 #define _NUM 4
 
+/* Declarations for ESC+FN+NUM Tap Dance*/
 typedef enum {
     TD_NONE,
     TD_UNKNOWN,
+    TD_SINGLE_TAP,
     TD_SINGLE_HOLD,
+    TD_DOUBLE_TAP,
     TD_DOUBLE_HOLD
 } td_state_t;
 
@@ -36,14 +39,15 @@ typedef struct {
 } td_tap_t;
 
 enum {
-    FN_NUM,
+    ESC_FN,
 };
 
 td_state_t cur_dance(tap_dance_state_t *state);
 
-void fn_finished(tap_dance_state_t *state, void *user_data);
-void fn_reset(tap_dance_state_t *state, void *user_data);
+void esc_fn_finished(tap_dance_state_t *state, void *user_data);
+void esc_fn_reset(tap_dance_state_t *state, void *user_data);
 
+/* Per layer RGB Matrix animation */
 layer_state_t layer_state_set_user(layer_state_t state) {
     switch (get_highest_layer(state)) {
         case _NUM:
@@ -68,6 +72,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     return state;
 }
 
+/* Determine the correct keycode for _DEF Home Row Modifiers */
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case GUI_T(KC_A):
@@ -110,24 +115,24 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[_DEF] = LAYOUT_65_ansi_blocker( /* Base */
-		QK_GESC,    KC_1,        KC_2,          KC_3,          KC_4,          KC_5,    KC_6,    KC_7,        KC_8,        KC_9,        KC_0,           KC_MINS, KC_EQL,  KC_ENT,  KC_TRNS,
+	[_DEF] = LAYOUT_65_ansi_blocker( /* Default */
+		KC_GRV,     KC_1,        KC_2,          KC_3,          KC_4,          KC_5,    KC_6,    KC_7,        KC_8,        KC_9,        KC_0,           KC_MINS, KC_EQL,  KC_ENT,  KC_TRNS,
 		KC_TAB,     KC_Q,        KC_W,          KC_E,          KC_R,          KC_T,    KC_Y,    KC_U,        KC_I,        KC_O,        KC_P,           KC_LBRC, KC_RBRC, KC_BSLS, KC_TRNS,
-		TD(FN_NUM), GUI_T(KC_A), ALT_T(KC_S),   SFT_T(KC_D),   CTL_T(KC_F),   KC_G,    KC_H,    CTL_T(KC_J), SFT_T(KC_K), ALT_T(KC_L), GUI_T(KC_SCLN), KC_QUOT,          KC_BSPC, TG(_ENG),
+		TD(ESC_FN), GUI_T(KC_A), ALT_T(KC_S),   SFT_T(KC_D),   CTL_T(KC_F),   KC_G,    KC_H,    CTL_T(KC_J), SFT_T(KC_K), ALT_T(KC_L), GUI_T(KC_SCLN), KC_QUOT,          KC_BSPC, TG(_ENG),
 		KC_LSFT,    KC_Z,        KC_X,          KC_C,          KC_V,          KC_B,    KC_N,    KC_M,        KC_COMM,     KC_DOT,      KC_SLSH,        KC_RSFT,          KC_UP,   TG(_GAM),
 		KC_LCTL,    KC_LGUI,     KC_LALT,                                     KC_SPC,                        KC_CAPS,                  KC_RALT,        KC_LEFT,          KC_DOWN, KC_RIGHT
 	),
 	[_GAM] = LAYOUT_65_ansi_blocker( /* 5xFN */
 		KC_ESC,     KC_TRNS,     KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 		KC_TRNS,    KC_TRNS,     KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-		TD(FN_NUM), KC_A,        KC_S,          KC_D,          KC_F,          KC_TRNS, KC_TRNS, KC_J,        KC_K,        KC_L,        KC_SCLN,        KC_TRNS,          KC_TRNS, TG(_ENG),
+		TD(ESC_FN), KC_A,        KC_S,          KC_D,          KC_F,          KC_TRNS, KC_TRNS, KC_J,        KC_K,        KC_L,        KC_SCLN,        KC_TRNS,          KC_TRNS, TG(_ENG),
 		KC_TRNS,    KC_TRNS,     KC_TRNS,       KC_TRNS,       KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,        KC_TRNS,          KC_TRNS, TG(_GAM),
 		KC_TRNS,    KC_TRNS,     KC_TRNS,                                     KC_SPC,                        KC_TRNS,                  KC_TRNS,        KC_TRNS,          KC_TRNS, KC_TRNS
 	),
     [_ENG] = LAYOUT_65_ansi_blocker( /* Engrammer */
-		QK_GESC,    KC_1,        KC_2,          KC_3,          KC_4,          KC_5,    KC_6,    KC_7,        KC_8,        KC_9,        KC_0,           KC_LBRC, KC_RBRC, KC_ENT,  KC_TRNS,
+		KC_GRV,     KC_1,        KC_2,          KC_3,          KC_4,          KC_5,    KC_6,    KC_7,        KC_8,        KC_9,        KC_0,           KC_LBRC, KC_RBRC, KC_ENT,  KC_TRNS,
 		KC_TAB,     KC_B,        KC_Y,          KC_O,          KC_U,          KC_QUOT, KC_SCLN, KC_L,        KC_D,        KC_W,        KC_V,           KC_Z,    KC_EQL,  KC_BSLS, KC_TRNS,
-		TD(FN_NUM), GUI_T(KC_C), ALT_T(KC_I),   SFT_T(KC_E),   CTL_T(KC_A),   KC_COMM, KC_DOT,  CTL_T(KC_H), SFT_T(KC_T), ALT_T(KC_S), GUI_T(KC_N),    KC_Q,             KC_BSPC, TG(_ENG),
+		TD(ESC_FN), GUI_T(KC_C), ALT_T(KC_I),   SFT_T(KC_E),   CTL_T(KC_A),   KC_COMM, KC_DOT,  CTL_T(KC_H), SFT_T(KC_T), ALT_T(KC_S), GUI_T(KC_N),    KC_Q,             KC_BSPC, TG(_ENG),
 		KC_LSFT,    KC_G,        KC_X,          KC_J,          KC_K,          KC_MINS, KC_SLSH, KC_R,        KC_M,        KC_F,        KC_P,           KC_RSFT,          KC_UP,   TG(_GAM),
 		KC_LCTL,    KC_LGUI,     KC_LALT,                                     KC_SPC,                        KC_CAPS,                  KC_RALT,        KC_LEFT,          KC_DOWN, KC_RIGHT
 	),
@@ -139,9 +144,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_NO,      KC_NO,       KC_NO,                                       QK_REP,                        KC_NO,                    KC_NO,          KC_MPRV,          KC_VOLD, KC_MNXT
 	),
     [_NUM] = LAYOUT_65_ansi_blocker( /* FN */
-		KC_NO,      KC_NO,       KC_NO,         KC_NO,         KC_NO,         KC_NO,   KC_NO,   KC_NO,       KC_NO,       KC_NO,       KC_NO,          KC_NO,   KC_NO,   KC_BSPC, KC_NO,
+		KC_NO,      KC_NO,       KC_NO,         KC_NO,         KC_NO,         KC_NO,   KC_NO,   KC_NO,       KC_NO,       KC_NO,       KC_NO,          KC_NO,   KC_NO,   KC_ENT,  KC_NO,
 		KC_NO,      KC_NO,       KC_NO,         KC_NO,         KC_NO,         KC_NO,   KC_TAB,  KC_P7,       KC_P8,       KC_P9,       KC_PPLS,        KC_PAST, KC_NO,   KC_NO,   KC_NO,
-		KC_NO,      KC_NO,       KC_NO,         KC_NO,         KC_NO,         KC_NO,   KC_NO,   KC_P4,       KC_P5,       KC_P6,       KC_PMNS,        KC_PSLS,          KC_ENT,  KC_NO,
+		KC_NO,      KC_NO,       KC_NO,         KC_NO,         KC_NO,         KC_NO,   KC_NO,   KC_P4,       KC_P5,       KC_P6,       KC_PMNS,        KC_PSLS,          KC_BSPC, KC_NO,
 		KC_NO,      KC_NO,       KC_NO,         KC_NO,         KC_NO,         KC_NO,   KC_NO,   KC_P1,       KC_P2,       KC_P3,       KC_PDOT,        KC_RSFT,          KC_NO,   KC_NO,
 		KC_NO,      KC_NO,       KC_NO,                                       KC_P0,                         KC_NO,                    KC_NO,          KC_NO,            KC_NO,   KC_NO
 	),
@@ -149,12 +154,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // Determine the current tap dance state
 td_state_t cur_dance(tap_dance_state_t *state) {
-    if (state->count < 2) {
-        if (state->pressed) return TD_SINGLE_HOLD;
-        else return TD_UNKNOWN;
-    } else if (state->count >= 2) {
-        if(state->pressed) return TD_DOUBLE_HOLD;
-        else return TD_UNKNOWN;
+    if (state->count == 1) {
+        if (!state->pressed) return TD_SINGLE_TAP; 
+        else return TD_SINGLE_HOLD; 
+    } else if (state->count == 2) {
+        if(!state->pressed) return TD_DOUBLE_TAP; 
+        else return TD_DOUBLE_HOLD;
     }
     else return TD_UNKNOWN;
 }
@@ -165,12 +170,19 @@ static td_tap_t fn_tap_state = {
     .state = TD_NONE
 };
 
-// Functions that control what our tap dance key does
-void fn_finished(tap_dance_state_t *state, void *user_data) {
+// Determines what the Tap Dance key outputs 
+void esc_fn_finished(tap_dance_state_t *state, void *user_data) {
     fn_tap_state.state = cur_dance(state);
     switch (fn_tap_state.state) {
+        case TD_SINGLE_TAP:
+            tap_code(KC_ESC);
+            break;
         case TD_SINGLE_HOLD:
             layer_on(_FN);
+            break;
+        case TD_DOUBLE_TAP:
+            tap_code(KC_ESC);
+            tap_code(KC_ESC);
             break;
         case TD_DOUBLE_HOLD:
             layer_on(_NUM);
@@ -180,8 +192,8 @@ void fn_finished(tap_dance_state_t *state, void *user_data) {
     }
 }
 
-void fn_reset(tap_dance_state_t *state, void *user_data) {
-    // If the key was held down and now is released then switch off the layer
+// Resets the Tap Dance key when KEY-UP
+void esc_fn_reset(tap_dance_state_t *state, void *user_data) {
     if (fn_tap_state.state == TD_SINGLE_HOLD) {
         layer_off(_FN);
     } else if (fn_tap_state.state == TD_DOUBLE_HOLD) {
@@ -191,7 +203,7 @@ void fn_reset(tap_dance_state_t *state, void *user_data) {
 }
 
 tap_dance_action_t tap_dance_actions[] = {
-    [FN_NUM] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, fn_finished, fn_reset)
+    [ESC_FN] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, esc_fn_finished, esc_fn_reset)
 };
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
@@ -201,4 +213,13 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
         default:
             return TAPPING_TERM;
     }
+}
+
+// Function to ignore certain keycodes when tapping QK_REPEAT_KEY
+bool remember_last_key_user(uint16_t keycode, keyrecord_t* record, uint8_t* remembered_mods) {
+    switch (keycode) {
+        case TD(ESC_FN):
+            return false;
+    }
+    return true;
 }
