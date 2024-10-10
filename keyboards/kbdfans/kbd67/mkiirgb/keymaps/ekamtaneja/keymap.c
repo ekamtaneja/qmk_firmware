bf16e37cc53199b04ecf9a8cc2fdea1b3df034c2
--get <95505981+ekamtaneja@users.noreply.github.com>
2024-10-10 13:26:01 -0700

    Added FN Layer mod tap

--- keyboards/kbdfans/kbd67/mkiirgb/keymaps/ekamtaneja/keymap.c
+++ keyboards/kbdfans/kbd67/mkiirgb/keymaps/ekamtaneja/keymap.c
@@ -113,7 +113,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_DEF] = LAYOUT_65_ansi_blocker( /* Default */
 		KC_GRV,     KC_1,        KC_2,        KC_3,        KC_4,        KC_5,    KC_6,    KC_7,        KC_8,        KC_9,        KC_0,           KC_MINS, KC_EQL,  KC_ENT,  KC_TRNS,
-		KC_TAB,     KC_Q,        KC_W,        KC_E,        KC_R,        KC_T,    KC_Y,    KC_U,        KC_I,        KC_O,        KC_P,           KC_LBRC, KC_RBRC, KC_BSLS, KC_TRNS,
+		KC_TAB,     KC_Q,        KC_W,        KC_E,        LT(_FN,KC_R),KC_T,    KC_Y,    KC_U,        KC_I,        KC_O,        KC_P,           KC_LBRC, KC_RBRC, KC_BSLS, KC_TRNS,
 		TD(ESC_FN), GUI_T(KC_A), ALT_T(KC_S), SFT_T(KC_D), CTL_T(KC_F), KC_G,    KC_H,    CTL_T(KC_J), SFT_T(KC_K), ALT_T(KC_L), GUI_T(KC_SCLN), KC_QUOT,          KC_BSPC, TG(_ENG),
 		KC_LSFT,    KC_Z,        KC_X,        KC_C,        KC_V,        KC_B,    KC_N,    KC_M,        KC_COMM,     KC_DOT,      KC_SLSH,        KC_RSFT,          KC_UP,   TG(_GAM),
 		KC_LCTL,    KC_LGUI,     KC_LALT,                               KC_SPC,                        KC_CAPS,                  KC_RALT,        KC_LEFT,          KC_DOWN, KC_RIGHT
@@ -127,7 +127,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	),
     [_ENG] = LAYOUT_65_ansi_blocker( /* Engrammer */
 		KC_GRV,     KC_1,        KC_2,        KC_3,        KC_4,        KC_5,    KC_6,    KC_7,        KC_8,        KC_9,        KC_0,           KC_LBRC, KC_RBRC, KC_ENT,  KC_TRNS,
-		KC_TAB,     KC_B,        KC_Y,        KC_O,        KC_U,        KC_QUOT, KC_SCLN, KC_L,        KC_D,        KC_W,        KC_V,           KC_Z,    KC_EQL,  KC_BSLS, KC_TRNS,
+		KC_TAB,     KC_B,        KC_Y,        KC_O,        LT(_FN,KC_U),KC_QUOT, KC_SCLN, KC_L,        KC_D,        KC_W,        KC_V,           KC_Z,    KC_EQL,  KC_BSLS, KC_TRNS,
 		TD(ESC_FN), GUI_T(KC_C), ALT_T(KC_I), SFT_T(KC_E), CTL_T(KC_A), KC_COMM, KC_DOT,  CTL_T(KC_H), SFT_T(KC_T), ALT_T(KC_S), GUI_T(KC_N),    KC_Q,             KC_BSPC, TG(_ENG),
 		KC_LSFT,    KC_G,        KC_X,        KC_J,        KC_K,        KC_MINS, KC_SLSH, KC_R,        KC_M,        KC_F,        KC_P,           KC_RSFT,          KC_UP,   TG(_GAM),
 		KC_LCTL,    KC_LGUI,     KC_LALT,                               KC_SPC,                        KC_CAPS,                  KC_RALT,        KC_LEFT,          KC_DOWN, KC_RIGHT
